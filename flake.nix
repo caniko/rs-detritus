ba86b5f565129901784d2aed7890c4b49f3d4782
Can H. Tartanoglu <gpg@rotas.mozmail.com>
2026-07-30 16:22:49 +0200

    feat: adopt rs-harbor toolchainProfile

--- flake.nix
+++ flake.nix
@@ -21,7 +21,7 @@
       url = "git+https://codeberg.org/caniko/plinth.git?ref=refs/heads/trunk";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-    rs-harbor.url = "git+https://codeberg.org/caniko/rs-harbor.git?ref=trunk&rev=9bfa8bdb0ecb22d7bc11448665f7fbaebae7a759";
+    rs-harbor.url = "git+https://codeberg.org/caniko/rs-harbor.git?ref=trunk&rev=c26b735eede8078f795651c4a9cbf0be8733b221";
   };
 
   outputs = {
@@ -42,9 +42,7 @@
         overlays = [(import rust-overlay)];
       };
       lib = pkgs.lib;
-      rustToolchain = pkgs.rust-bin.stable.latest.default.override {
-        extensions = ["rustfmt" "clippy"];
-      };
+      rustToolchain = rs-harbor.lib.mkToolchain { toolchainProfile = "nightly"; };
       craneLib = (crane.mkLib pkgs).overrideToolchain rustToolchain;
       cross = rs-harbor.lib.mkCross {
         inherit pkgs system;
